3bcc2db1b14d05794032c1b3cf1ac0eeb4fcdbe9
I477212 <cbruikman@gmail.com>
2025-06-16 17:47:04 +0200

    fix: correct route element for home and update BronnenBeheer path

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from "./pages/Index";
 import Agenda from "./pages/Agenda";
 import NotFound from "./pages/NotFound";
@@ -17,10 +17,9 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<Home />} />
           <Route path="/agenda" element={<Agenda />} />
-          <Route path="/bronnen-beheer" element={<BronnenBeheer />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/bronnenbeheer" element={<BronnenBeheer />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
